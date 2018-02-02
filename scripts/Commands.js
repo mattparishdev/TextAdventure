66c8e7d984a83334b8b5382748e21d0b045880cc
Matt Parish <mattparishdev@users.noreply.github.com>
2018-02-01 22:22:43 -0500

    Implemented help
    Finished up help implementation and argument parsing.

--- scripts/Commands.js
+++ scripts/Commands.js
@@ -1,173 +1,334 @@
-const CommandParam = (
-  function ()
+/***
+ * Map of available console command
+ * @type {Object.<string, Command>}
+ */
+const Commands = (
+  () =>
   {
-    function parseArgument(value, type)
-    {
-      let retVal = null;
-
-      switch(type)
+    /***
+     * @type {object} CommandParam
+     * @desc Object containing functionality for a console command parameter
+     * @property {function} getName
+     * @property {function} getType
+     * @property {function} hasDefault
+     * @property {function} getDefault
+     * @property {function} parseArgument
+     * @constructor
+     */
+    const CommandParam = (
+      () =>
       {
-        case "string":
-          retVal = value;
-          break;
-        case "int":
-          const parsedInt = Number.parseInt(value);
-          if(isNaN(parsedInt) === false)
-          {
-            retVal = parsedInt;
-          }
-          break;
-        case "float":
-          const parsedFloat = Number.parseFloat(value);
-          if(isNan(parsedFloat) === false)
-          {
-            retVal = parsedFloat;
-          }
-          break;
-      }
-
-      return retVal;
-    }
-
-    return function (name, type, defaultValue)
-    {
-      return {
-        getName: () => name,
-        getType: () => type,
-        hasDefault: () => defaultValue !== undefined,
-        getDefault: () => defaultValue,
-        parseArgument: (argument) =>
+        /***
+         * @private
+         * @desc Parses raw string arguments to the expected type
+         * @param {string} value - The raw string to be parsed to the desired type
+         * @param {string} type - The type to parse the string to
+         * @returns {*} - The parsed type, if parsed successfully; null otherwise
+         */
+        function parseArgument(value, type)
         {
-          let retVal = defaultValue;
+          let retVal = null;
 
-          if(argument !== undefined)
+          switch(type)
           {
-            retVal = parseArgument(argument, type);
+            case "string":
+              retVal = value;
+              break;
+            case "int":
+              const parsedInt = Number.parseInt(value);
+              if(isNaN(parsedInt) === false)
+              {
+                retVal = parsedInt;
+              }
+              break;
+            case "float":
+              const parsedFloat = Number.parseFloat(value);
+              if(isNan(parsedFloat) === false)
+              {
+                retVal = parsedFloat;
+              }
+              break;
           }
 
           return retVal;
         }
-      };
-    };
-  }
-)();
 
-const Command = (
-  function ()
-  {
-    function parseRequest(rawRequest)
-    {
-      let parsedRequest = [];
-
-      rawRequest.replace(/-(\D+?)\s+?(".+"|\d+\.?\d+)/g, function (match, g1, g2)
-      {
-        parsedRequest.push({[g1]: g2});
-      });
-
-      return parsedRequest;
-    }
-
-    function getCommandArguments(parsedRequest, cmdParams)
-    {
-      let retVal =
+        /***
+         * @private
+         * @constructor
+         * @param {string} name - The name of this command parameter
+         * @param {string} type - The type of this command parameter
+         * @param {*} [defaultValue=undefined] - The default value of this command parameter, if it is optional
+         * @returns {CommandParam}
+         */
+        return function (name, type, defaultValue)
         {
-          cmdArgs: [],
-          errors: []
+          return {
+            /***
+             * Retrieve the name of this command parameter
+             * @returns {string}
+             */
+            getName: () => name,
+
+            /***
+             * Retrieve the type of this command parameter
+             * @returns {string}
+             */
+            getType: () => type,
+
+            /***
+             * Check if this command parameter has a default value
+             * @returns {boolean}
+             */
+            hasDefault: () => defaultValue !== undefined,
+
+            /***
+             * Retrieve the default value of this command parameter
+             * @returns {*}
+             */
+            getDefault: () => defaultValue,
+
+            /***
+             * Parse an argument passed for this command parameter
+             * @param {string} argument - The string representation of the argument
+             * @returns {*} -
+             *  The type associated with this command parameter, if it was able to be parsed; null otherwise
+             */
+            parseArgument: (argument) =>
+            {
+              let retVal = defaultValue;
+
+              if(argument !== undefined)
+              {
+                retVal = parseArgument(argument, type);
+              }
+
+              return retVal;
+            }
+          };
         };
+      }
+    )();
 
-      cmdParams.forEach(function (value)
+    /***
+     * @type {object} Command
+     * @desc Functionality and representation of a console command
+     * @property {function} execute
+     * @property {function} getHelp
+     * @constructor
+     */
+    const Command = (
+      () =>
       {
-        const rawArg = parsedRequest[value.getName()];
-
-        if(rawArg !== undefined)
+        /***
+         * @private
+         * @desc Parses the raw request into raw key/value pair command arguments for this command
+         * @param {string} rawRequest - The string containing the command arguments
+         * @returns {Object.<string, string>} -
+         *  A map of the parsed command arguments, with the command name as key and the command argument as the
+         *  value
+         */
+        function parseRequest(rawRequest)
         {
-          const parsedArg = value.parseArgument(rawArg);
+          let parsedRequest = {};
 
-          if(parsedArg != null)
+          rawRequest.replace(/-(\D+?)\s+?(".+"|\D[^\s]+|\d+\.?\d*)/g, (match, g1, g2) =>
           {
-            retVal.cmdArgs.push(parsedArg);
-          }
-          else
-          {
-            retVal.errors.push(`Parameter ${value.getName()} was not of expected type ${value.getType()}`);
-          }
+            parsedRequest[g1] = g2;
+          });
+
+          return parsedRequest;
         }
-        else if(rawArg.hasDefault())
+
+        /***
+         * @private
+         * @desc Retrieves the parsed command arguments
+         * @param {Object.<string, string>[]} parsedRequest - The array of raw key/value pair command arguments
+         * @param {CommandParam[]} cmdParams - The list of command parameters for this command
+         * @returns {{cmdArgs: *[], errors: string[]}}
+         */
+        function getCommandArguments(parsedRequest, cmdParams)
         {
-          retVal.cmdArgs.push(rawArg.getDefault());
-        }
-        else
-        {
-          retVal.errors.push(`Required parameter ${value.getName()} was not provided`);
-        }
-      });
+          let retVal =
+            {
+              cmdArgs: [],
+              errors: []
+            };
 
-      return retVal;
-    }
-
-    return function (helpText, execute, cmdParams = [])
-    {
-      const _helpText = helpText;
-      const _cmdParams = cmdParams;
-
-      return {
-        execute: (rawRequest) =>
-        {
-          const parsedRequest = parseRequest(rawRequest.trim());
-          const cmdArgsResult = getCommandArguments(parsedRequest, _cmdParams);
-
-          if(cmdArgsResult.errors.length === 0)
+          cmdParams.forEach((value) =>
           {
-            execute(...cmdArgsResult.cmdArgs);
-          }
-          else
-          {
-            // TODO: handle incorrect argument errors
-            alert(cmdArgsResult.errors);
-          }
-        },
-        help: () => _helpText
-      };
-    };
-  }
-)();
+            const rawArg = parsedRequest[value.getName()];
 
-const Commands = (
-  function ()
-  {
-    const _helpCommand = new Command(
-      "Get information about the available commands, or about a specific command",
-      function (commandName)
-      {
-        alert(`Help: ${commandName}`);
-      },
-      [
-        new CommandParam("command", "string")
-      ]
-    );
+            if(rawArg !== undefined)
+            {
+              const parsedArg = value.parseArgument(rawArg);
 
+              if(parsedArg != null)
+              {
+                retVal.cmdArgs.push(parsedArg);
+              }
+              else
+              {
+                retVal.errors.push(`Parameter ${value.getName()} was not of expected type ${value.getType()}`);
+              }
+            }
+            else if(value.hasDefault())
+            {
+              retVal.cmdArgs.push(value.getDefault());
+            }
+            else
+            {
+              retVal.errors.push(`Required parameter ${value.getName()} was not provided`);
+            }
+          });
+
+          return retVal;
+        }
+
+        /***
+         * @private
+         * @param {string} name - The name of this command
+         * @param {string} helpText - The text to be displayed to the user when help is requested
+         * @param {function} execute - The function to be used when the command is requested
+         * @param {CommandParam[]} - The list of command parameters that this command expects
+         * @constructor
+         * @returns {Command}
+         */
+        return function Command(name, helpText, execute, cmdParams = [])
+        {
+          return {
+            /***
+             * Execute the command
+             * @param {string} rawRequest - The string containing the raw command arguments and identifiers
+             * @returns {string|null} - The command response, if one is necessary; null otherwise
+             */
+            execute: (rawRequest) =>
+            {
+              const parsedRequest = parseRequest(rawRequest.trim());
+              const cmdArgsResult = getCommandArguments(parsedRequest, cmdParams);
+
+              let response = null;
+
+              if(cmdArgsResult.errors.length === 0)
+              {
+                response = execute(...cmdArgsResult.cmdArgs);
+              }
+              else
+              {
+                // TODO: handle formatting incorrect argument errors
+                response = cmdArgsResult.errors;
+              }
+
+              return response;
+            },
+
+            /***
+             * Retrieve the brief help text for this command
+             * @returns {string}
+             */
+            getHelpBrief: () => `<strong>${name}</strong>: ${helpText}<br/>`,
+
+            /***
+             * Retrieve the expanded help text for this command, including parameter names, types, and usage example
+             * @returns {string}
+             */
+            getHelpExpanded: function()
+            {
+              // TODO: implement expanded help
+              return this.getHelpBrief();
+            }
+          };
+        };
+      }
+    )();
+
+    /***
+     * Command to clear the console screen
+     */
     const _clsCommand = new Command(
-      "Clear the screen",
-      function ()
+      "cls",
+      "Clear the console",
+      () =>
       {
-        // Set a timeout of 5ms in order to clear the screen AFTER the previous command has been added
-        setTimeout(function ()
+        // Set a timeout of 3ms in order to clear the screen AFTER the previous command has been added
+        setTimeout(() =>
         {
           const input = $("#input");
           const activeConsole = $("#activeConsole");
 
           input.html("");
           activeConsole.siblings().remove();
-        }, 5);
+        }, 3);
 
-        return "";
+        return null;
       }
     );
 
+    /***
+     * @private
+     * @type {Object.<string,Command>}
+     * @desc The map of all available non-help commands
+     */
+    const _nonHelpCommands =
+      {
+        cls: _clsCommand
+      };
+
+    /***
+     * Command to get help and list either all commands available, or for a specific command
+     */
+    const _helpCommand = new Command(
+      "help",
+      "Get information about the available commands, or about a specific command",
+      (commandName) =>
+      {
+        /***
+         * Get a list of all commands' brief help text
+         * @returns {string}
+         */
+        function listAllCommands()
+        {
+          let result = "";
+
+          Reflect.ownKeys(_nonHelpCommands).forEach((value) =>
+          {
+            result += `<br/>${_nonHelpCommands[value].getHelpBrief()}`;
+          });
+
+          return result;
+        }
+
+        let response = null;
+
+        if(commandName.length > 0)
+        {
+          const cmd = _nonHelpCommands[commandName];
+
+          if(cmd !== undefined)
+          {
+            response = cmd.getHelpExpanded();
+          }
+          else
+          {
+            response = `<strong>${commandName}</strong>&nbsp;is not an available command<br/>${listAllCommands()}`;
+          }
+        }
+        else
+        {
+          response = listAllCommands();
+        }
+
+        return response;
+      },
+      [
+        new CommandParam("command", "string", "")
+      ]
+    );
+
     return {
+      ..._nonHelpCommands,
       help: _helpCommand,
-      "?": _helpCommand,
-      cls: _clsCommand
+      "?": _helpCommand
     };
   }
 )();
--- scripts/site.js
+++ scripts/site.js
@@ -1,262 +1,99 @@
 $.when(
   $.getScript("scripts/KeyCodes.js"),
+  $.getScript("scripts/Commands.js"),
   $.Deferred((deferred) =>
   {
-    $( deferred.resolve );
+    $(deferred.resolve);
   })
 ).done(() =>
 {
-  const CommandParam = (
-    () =>
-    {
-      function parseArgument(value, type)
-      {
-        let retVal = null;
-
-        switch(type)
-        {
-          case "string":
-            retVal = value;
-            break;
-          case "int":
-            const parsedInt = Number.parseInt(value);
-            if(isNaN(parsedInt) === false)
-            {
-              retVal = parsedInt;
-            }
-            break;
-          case "float":
-            const parsedFloat = Number.parseFloat(value);
-            if(isNan(parsedFloat) === false)
-            {
-              retVal = parsedFloat;
-            }
-            break;
-        }
-
-        return retVal;
-      }
-
-      return function (name, type, defaultValue)
-      {
-        return {
-          getName: () => name,
-          getType: () => type,
-          hasDefault: () => defaultValue !== undefined,
-          getDefault: () => defaultValue,
-          parseArgument: (argument) =>
-          {
-            let retVal = defaultValue;
-
-            if(argument !== undefined)
-            {
-              retVal = parseArgument(argument, type);
-            }
-
-            return retVal;
-          }
-        };
-      };
-    }
-  )();
-
-  const Command = (
-    () =>
-    {
-      function parseRequest(rawRequest)
-      {
-        let parsedRequest = [];
-
-        rawRequest.replace(/-(\D+?)\s+?(".+"|\d+\.?\d+)/g, function (match, g1, g2)
-        {
-          parsedRequest.push({[g1]: g2});
-        });
-
-        return parsedRequest;
-      }
-
-      function getCommandArguments(parsedRequest, cmdParams)
-      {
-        let retVal =
-          {
-            cmdArgs: [],
-            errors: []
-          };
-
-        cmdParams.forEach(function (value)
-        {
-          const rawArg = parsedRequest[value.getName()];
-
-          if(rawArg !== undefined)
-          {
-            const parsedArg = value.parseArgument(rawArg);
-
-            if(parsedArg != null)
-            {
-              retVal.cmdArgs.push(parsedArg);
-            }
-            else
-            {
-              retVal.errors.push(`Parameter ${value.getName()} was not of expected type ${value.getType()}`);
-            }
-          }
-          else if(value.hasDefault())
-          {
-            retVal.cmdArgs.push(value.getDefault());
-          }
-          else
-          {
-            retVal.errors.push(`Required parameter ${value.getName()} was not provided`);
-          }
-        });
-
-        return retVal;
-      }
-
-      return function (helpText, execute, cmdParams = [])
-      {
-        return {
-          execute: (rawRequest) =>
-          {
-            const parsedRequest = parseRequest(rawRequest.trim());
-            const cmdArgsResult = getCommandArguments(parsedRequest, cmdParams);
-
-            if(cmdArgsResult.errors.length === 0)
-            {
-              execute(...cmdArgsResult.cmdArgs);
-            }
-            else
-            {
-              // TODO: handle incorrect argument errors
-              alert(cmdArgsResult.errors);
-            }
-          },
-          getHelp: () => helpText
-        };
-      };
-    }
-  )();
-
-  const Commands = (
-    () =>
-    {
-      const _helpCommand = new Command(
-        "Get information about the available commands, or about a specific command",
-        function (commandName)
-        {
-          alert(`Help: ${commandName}`);
-        },
-        [
-          new CommandParam("command", "string")
-        ]
-      );
-
-      const _clsCommand = new Command(
-        "Clear the screen",
-        function ()
-        {
-          // Set a timeout of 5ms in order to clear the screen AFTER the previous command has been added
-          setTimeout(() =>
-          {
-            const input = $("#input");
-            const activeConsole = $("#activeConsole");
-
-            input.html("");
-            activeConsole.siblings().remove();
-          }, 3);
-
-          return "";
-        }
-      );
-
-      return {
-        help: _helpCommand,
-        "?": _helpCommand,
-        cls: _clsCommand
-      };
-    }
-  )();
-
   const input = $("#input");
   const activeConsole = $("#activeConsole");
+
+  let prevCmds = [];
+  let prevCmdIndex = -1;
 
   setupConsole();
 
   function setupConsole()
   {
-    // Give console input focus
     input.focus();
+    input.keypress(commandHandler);
+    input.keydown(previousCommandHandler);
+  }
 
-    let prevCmds = [];
-    let prevCmdIndex = -1;
-
-    // Command handler
-    input.keypress((event) =>
+  /***
+   * Handles the ability to enter command requests, process them, and display results
+   * @param event - The event that triggered this handler
+   */
+  function commandHandler(event)
+  {
+    console.log(`keypress: ${event.which}`);
+    if(event.which === KeyCodes.Enter && event.shiftKey === false)
     {
-      console.log(`keypress: ${event.which}`);
-      if(event.which === KeyCodes.Enter && event.shiftKey === false)
+      // Don't add the new line
+      event.preventDefault();
+
+      // Process the command
+      const request = input.html().trim();
+      let response = "";
+
+      if(request.length > 0)
       {
-        // Don't add the new line
-        event.preventDefault();
+        prevCmds.unshift(request);
 
-        // Process the command
-        const request = input.html().trim();
-        let response = "";
+        const consoleRequest = parseRequest(request);
 
-        if(request.length > 0)
+        if(consoleRequest !== null)
         {
-          prevCmds.unshift(request);
-
-          const consoleRequest = parseRequest(request);
-
-          if(consoleRequest !== null)
-          {
-            response = processRequest(consoleRequest);
-          }
+          response = processRequest(consoleRequest);
         }
-
-        displayResult(request, response);
       }
-    });
 
-    // Previous Command Handler
-    input.keydown((event) =>
+      displayResult(request, response);
+    }
+  }
+
+  /***
+   * Handles the ability to recall previously entered commands
+   * @param event - The event that triggered this handler
+   */
+  function previousCommandHandler(event)
+  {
+    console.log(`keydown: ${event.which}`);
+    if(event.which === KeyCodes.UpArrow)
     {
-      console.log(`keydown: ${event.which}`);
-      if(event.which === KeyCodes.UpArrow)
+      event.preventDefault();
+
+      let newHtml = "";
+
+      if(prevCmdIndex < prevCmds.length - 1)
       {
-        event.preventDefault();
-
-        let newHtml = "";
-
-        if(prevCmdIndex < prevCmds.length - 1)
-        {
-          ++prevCmdIndex;
-        }
-
-        if(prevCmdIndex > -1)
-        {
-          newHtml = prevCmds[prevCmdIndex];
-        }
-
-        input.html(newHtml);
+        ++prevCmdIndex;
       }
-      else if(event.which === KeyCodes.DownArrow && prevCmdIndex > 0)
+
+      if(prevCmdIndex > -1)
       {
-        event.preventDefault();
-
-        --prevCmdIndex;
-
-        input.html(prevCmds[prevCmdIndex]);
+        newHtml = prevCmds[prevCmdIndex];
       }
-      else
+
+      input.html(newHtml);
+    }
+    else if(event.which === KeyCodes.DownArrow && prevCmdIndex > 0)
+    {
+      event.preventDefault();
+
+      --prevCmdIndex;
+
+      input.html(prevCmds[prevCmdIndex]);
+    }
+    else
+    {
+      if(prevCmdIndex > -1)
       {
-        if(prevCmdIndex > -1)
-        {
-          prevCmdIndex = -1;
-        }
+        prevCmdIndex = -1;
       }
-    });
+    }
   }
 
   /***
@@ -300,19 +137,20 @@ $.when(
    */
   function processRequest(request)
   {
-    let result = "";
+    let response = null;
     const command = Commands[request.command];
 
     if(command !== undefined)
     {
-      result = command.execute(request.arguments);
+      response = command.execute(request.arguments);
     }
     else
     {
-      result = "The requested command is unavailable. Consider using&nbsp;<strong>help</strong>";
+      response = `<strong>${request.command}</strong>&nbsp;is not an available command. Consider using&nbsp;
+        <strong>help</strong>&nbsp;to get a list of available commands.`;
     }
 
-    return result;
+    return response !== null ? response : "";
   }
 
   /***
